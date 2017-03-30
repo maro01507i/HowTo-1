54585046f1965a6467af61a9670dd8f882bf992f
alvaromanja1 <alvaromanja1@opendeusto.es>
2017-03-30 11:18:39 +0200

    Rama remota Barbarian.java

--- src/main/java/game/Barbarian.java
+++ src/main/java/game/Barbarian.java
@@ -25,5 +25,6 @@ logger.info("Estudiante B añade lineas en medio para conflicto");
 		logger.info("Modificacion realizada desde Github simulando cambios hechos por el estudiante A.");
 logger.info("Estudiante A metemos debug; otro cambio de linea -linea 2 extra para conflicto.");
 		logger.info("Elfo por un lado barbarian por otro.");
+		//Simulamos rama remota
 	}
 }
