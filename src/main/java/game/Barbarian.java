aea8f1e0469d4fde698362e6b3c7c13be1d854ee
alvaromanja1 <alvaromanja1@opendeusto.es>
2017-03-30 10:53:50 +0200

    Provocar conflicto

--- src/main/java/game/Barbarian.java
+++ src/main/java/game/Barbarian.java
@@ -23,6 +23,6 @@ public class Barbarian extends Character {
 		logger.info("You are a strong, hulky barbarian!!!.");
 logger.info("Estudiante B añade lineas en medio para conflicto"); 
 		logger.info("Modificacion realizada desde Github simulando cambios hechos por el estudiante A.");
-		logger.info("Estudiante A -linea 2 extra para conflicto.");
+		logger.info("Estudiante A otro cambio de linea -linea 2 extra para conflicto.");
 	}
 }
